796b6fb0c08f90b64b6b5a3ee93acee148bd494b
Rachel Austin <rachelaustin12@gmail.com>
2026-04-27 09:19:58 +0000

    Visual edits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,9 +17,9 @@ export default function Home() {
         <h1 className="font-nunito font-bold text-4xl sm:text-5xl mb-3" style={{ color: '#0ea5e9' }}>
           Where's the Ice Cream Van?
         </h1>
-        <p className="font-nunito mb-12 text-lg hidden" style={{ color: '#f472b6' }}>
-          Are you a driver or an ice cream hunter?
-        </p>
+        
+
+        
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {/* Hunter */}
